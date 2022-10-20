5d3078633d20edc3764668030c582033558956ca
paulo.fernandes <paulo.fernandes@55pbx.com.br>
2022-10-20 11:29:58 -0300

    configutating webpack

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname,'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.(js|ts)?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/env"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'eval-source-map'
+
+}
